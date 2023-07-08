50ac41439fb01ffedb5fa84429082ca09240e077
Naveed Zaynali <promisezey@gmail.com>
2023-07-08 07:51:17 +0330

    comit

---
+++ doread.py
@@ -0,0 +1,19 @@
+import pathlib
+
+FILE = '../../pythonProject/projects_tempts/differnet/webs/data.txt'
+
+
+def reader(filepath=FILE):
+    with open(filepath, 'r') as locally:
+        items = locally.readlines()
+    return items
+
+
+def writer(items, filepath=FILE):
+    with open(filepath, 'w') as inner:
+        inner.writelines(items)
+
+
+if __name__ == '__main__':
+    make = reader()
+    print(make)
